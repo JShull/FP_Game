8d6af268ea5a8a7287b59f15cd2d938557346ed6
John Shull <the.john.shull@gmail.com>
2025-04-04 02:54:20 -0400

    Setting up 3D and 2D Right / Left Screen Side
    Example updates

--- Samples/SamplesURP/Scripts/FPGameManager_ToolExample.cs
+++ Samples/SamplesURP/Scripts/FPGameManager_ToolExample.cs
@@ -11,9 +11,12 @@ namespace FuzzPhyte.Game.Samples
     {
         [Space]
         [Header("FP Game Manager Tool Example")]
-        public FPGameUIMouseListener fPGameUIMouseListener;
-        public List<FP_Tool<FP_MeasureToolData>> AllMeasureTools = new List<FP_Tool<FP_MeasureToolData>>();
-        public Button TheMeasureToolButton;
+        public FPGameUIMouseListener fPGameUIMouseListenerLeftScreen;
+        public FPGameUIMouseListener fPGameUIMouseListenerRightScreen;
+        public List<FP_Tool<FP_MeasureToolData>> AllMeasureToolsLeft = new List<FP_Tool<FP_MeasureToolData>>();
+        public List<FP_Tool<FP_MeasureToolData>> AllMeasureToolsRight = new List<FP_Tool<FP_MeasureToolData>>();
+        public Button TheMeasureTool2DButton;
+        public Button TheMeasureTool3DButton;
         public UnityEvent OnUnityGamePausedEvent;
         public UnityEvent OnUnityGameUnPausedEvent;
        
@@ -21,28 +24,7 @@ namespace FuzzPhyte.Game.Samples
         public override void Start()
         {
             base.Start();
-            //register my listeners
-            if (fPGameUIMouseListener != null)
-            {
-                /*
-                for(int i = 0; i < AllMeasureTools.Count; i++)
-                {
-                    var aTool = AllMeasureTools[i];
-                    IFPUIEventListener<FP_Tool<FP_MeasureToolData>> castedInterface = aTool as IFPUIEventListener<FP_Tool<FP_MeasureToolData>>;
-                    if (castedInterface!=null)
-                    {
-                        //has the interface?
-                        fPGameUIMouseListener.RegisterListener(castedInterface);
-                    }
-                  
-                }
-                */
-                
-            }
-            else
-            {
-                Debug.LogError($"Probably need a mouse listener for our UI Events!");
-            }
+            
         }
         public override void FixedUpdate()
         {
@@ -76,9 +58,13 @@ namespace FuzzPhyte.Game.Samples
             GameClock.TheClock.StartClockReporter();
             base.StartEngine();
            
-            if (TheMeasureToolButton != null)
+            if (TheMeasureTool2DButton != null)
             {
-               TheMeasureToolButton.interactable = true;
+               TheMeasureTool2DButton.interactable = true;
+            }
+            if(TheMeasureTool3DButton!=null)
+            {
+                TheMeasureTool3DButton.interactable = true;
             }
             // we want to stop make sure our overview UI isn't blocking our Tools requirements (OnDrag etc)
         }
@@ -93,9 +79,13 @@ namespace FuzzPhyte.Game.Samples
         public override void StopEngine()
         {
             base.StopEngine();
-            if (TheMeasureToolButton != null)
+            if (TheMeasureTool2DButton != null)
             {
-                TheMeasureToolButton.interactable = false;
+                TheMeasureTool2DButton.interactable = false;
+            }
+            if(TheMeasureTool3DButton!=null)
+            {
+                TheMeasureTool3DButton.interactable = false;
             }
         }
         public override void PauseEngine()
@@ -118,11 +108,11 @@ namespace FuzzPhyte.Game.Samples
             var theTool = thePassedTool.GetComponent<FP_Tool<FP_MeasureToolData>>();
             if (theTool != null)
             {
-                if (AllMeasureTools.Contains(theTool))
+                if (AllMeasureToolsLeft.Contains(theTool))
                 {
                     //set the current tool
-                    fPGameUIMouseListener.SetCurrentEngagedData(theTool);
-                    fPGameUIMouseListener.SetCurrentEngagedGameObject(theTool.gameObject);
+                    fPGameUIMouseListenerLeftScreen.SetCurrentEngagedData(theTool);
+                    fPGameUIMouseListenerLeftScreen.SetCurrentEngagedGameObject(theTool.gameObject);
                     //activate the tool with it's own data
                     theTool.Initialize(theTool.ToolData);
                     theTool.ActivateTool();
@@ -131,12 +121,54 @@ namespace FuzzPhyte.Game.Samples
                     if (castedInterface != null)
                     {
                         //tell our mouse listener to register the current tool
-                        fPGameUIMouseListener.RegisterListener(castedInterface);
+                        fPGameUIMouseListenerLeftScreen.RegisterListener(castedInterface);
                         //I want to tell the tool to Unregister itself with the mouse listener when it's deactivated
                         theTool.OnDeactivated += (tool) =>
                         {
-                            fPGameUIMouseListener.UnregisterListener(castedInterface);
-                            fPGameUIMouseListener.ResetCurrentEngagedData();
+                            fPGameUIMouseListenerLeftScreen.UnregisterListener(castedInterface);
+                            fPGameUIMouseListenerLeftScreen.ResetCurrentEngagedData();
+                        };
+                    }
+
+                }
+                else
+                {
+                    Debug.LogError($"The Tool {theTool} is not in the list of tools!");
+                }
+            }
+            else
+            {
+                Debug.LogError($"The GameObject you passed me, {thePassedTool.name}, does not have a FP_Tool<FP_MeasureToolData> component on it!");
+            }
+        }
+        /// <summary>
+        /// Public UI function to be called from the UI Button
+        /// </summary>
+        /// <param name="thePassedTool">GameObject that contains FP_Tool</param>
+        public void UI3DToolButtonPushed(GameObject thePassedTool)
+        {
+            var theTool = thePassedTool.GetComponent<FP_Tool<FP_MeasureToolData>>();
+            if (theTool != null)
+            {
+                if (AllMeasureToolsRight.Contains(theTool))
+                {
+                    //set the current tool
+                    fPGameUIMouseListenerRightScreen.SetCurrentEngagedData(theTool);
+                    fPGameUIMouseListenerRightScreen.SetCurrentEngagedGameObject(theTool.gameObject);
+                    //activate the tool with it's own data
+                    theTool.Initialize(theTool.ToolData);
+                    theTool.ActivateTool();
+                    //register the tool 
+                    IFPUIEventListener<FP_Tool<FP_MeasureToolData>> castedInterface = theTool as IFPUIEventListener<FP_Tool<FP_MeasureToolData>>;
+                    if (castedInterface != null)
+                    {
+                        //tell our mouse listener to register the current tool
+                        fPGameUIMouseListenerRightScreen.RegisterListener(castedInterface);
+                        //I want to tell the tool to Unregister itself with the mouse listener when it's deactivated
+                        theTool.OnDeactivated += (tool) =>
+                        {
+                            fPGameUIMouseListenerRightScreen.UnregisterListener(castedInterface);
+                            fPGameUIMouseListenerRightScreen.ResetCurrentEngagedData();
                         };
                     }
 
